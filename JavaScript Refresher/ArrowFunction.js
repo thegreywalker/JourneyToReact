7f6a9f8357ab81981e4987b18b1b1c47639cf6bb
iamapurba2003 <new.to.world2003@gmail.com>
2021-08-18 23:25:25 +0530

    Added Contents of ArrowFunction.js file along with the Code

---
+++ JavaScript Refresher/ArrowFunction.js
@@ -0,0 +1,35 @@
+// In JavaScript we have many function but mainly we will learn about "function" and Arrow function 
+
+// function Syntax 
+
+function myname(names) {
+    console.log(names);
+}
+myname("Apurba");
+
+// You can also Store this function in Object as well like in Variables
+
+let myVar = function hello(args) {
+    console.log(`Hey ${args}`);
+}
+// Or
+const myfunc = function hey(args) {
+    console.log(`Hey ${args}`);
+}
+
+// Arrow function Syntax
+let myVar = (args) => {
+    console.log(`Hey ${args}`);
+}
+
+// Or
+
+const myfunc = (args) => {
+    console.log(`Hey ${args}`);
+}
+
+// Arrow function was created to solve the Problem of n`this` Keyword .. It Becomes important when you assign the function to Objects like"Variables". `this` Keyword in JavaScript refers to the window Object 
+
+// Let's see a Arrow function rertuen something
+const multiply = number => number * 2;
+console.log(multiply(4))
