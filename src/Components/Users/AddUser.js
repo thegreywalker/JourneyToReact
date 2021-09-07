3e4a7c5dffe74723826961c36feb43e5cc1378ef
Apurba Ghosh <new.to.world2003@gmail.com>
2021-09-07 12:10:22 +0530

    Added custom Card & Button Component made by me and Overall Styling of the Form which takes Username and Age as Input and also added their Styles

---
+++ src/Components/Users/AddUser.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+import styles from "./AddUser.module.css"
+
+const AddUser = (props) => {
+    const addUserHandler = (event) => {
+        event.preventDefault();
+    };
+    return (
+        <Card className={styles.input}>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">Username</label>
+                <input id="username" type="text" />
+                <label htmlFor="age">Age (Years)</label>
+                <input type="number" id="age" />
+                <Button type="button"> Add User</Button>
+            </form>
+        </Card>
+    );
+};
+
+export default AddUser;
