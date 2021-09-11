c629a3128f06a0bc596a57ade4e56f6220980991
Apurba Ghosh <new.to.world2003@gmail.com>
2021-09-11 13:41:57 +0530

    Added A Wrapper Component and understood how to work with Portals and Fragments ... Wherein Fragments are a way we can aboid div Soup in HTML DOM Structure which is the final code into which the React Code get's translated into ... Also Portals are a way we can render our Components into different root elements by defining them in the index.html file in the Public Folder ... Finally, Refs are a way we actually kinda managae our state it basically references thw previous state of the element in the Browser and the updates it accordingly like the input boxes where rather than using State and Updating the Value with each Keystrokes we refrence it using Ref and then Update it accordingly ... Although, the edge case of using ref is that we have handle the DOM elements by ourselves like to reset the Value of the Input feilds I had to use DOM APIs given by the Browser specifically document.getElementByID() here in case of the Input fields ... The Section Ends here

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,50 +1,48 @@
-import React, { useState } from 'react';
-
+import React, { useState, useRef } from 'react';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import ErrorModal from '../UI/ErrorModal';
+import Wrapper from '../Helpers/Wrapper';
 import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
+
+  const namedInputRef = useRef();
+  const ageInputRef = useRef();
+
   const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    const enteredName = namedInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
       setError({
         title: 'Invalid input',
         message: 'Please enter a valid name and age (non-empty values).',
       });
       return;
     }
-    if (+enteredAge < 1) {
+    if (+enteredUserAge < 1) {
       setError({
         title: 'Invalid age',
         message: 'Please enter a valid age (> 0).',
       });
       return;
     }
-    props.onAddUser(enteredUsername, enteredAge);
-    setEnteredUsername('');
-    setEnteredAge('');
+    props.onAddUser(enteredName, enteredUserAge);
+    namedInputRef.current.value = "";
+    ageInputRef.current.value = "";
   };
 
-  const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
-  };
-
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
-  };
+  
 
   const errorHandler = () => {
     setError(null);
   };
 
   return (
-    <div>
+    <Wrapper>
       {error && (
         <ErrorModal
           title={error.title}
@@ -58,20 +56,18 @@ const AddUser = (props) => {
           <input
             id="username"
             type="text"
-            value={enteredUsername}
-            onChange={usernameChangeHandler}
+            ref={namedInputRef}
           />
           <label htmlFor="age">Age (Years)</label>
           <input
             id="age"
             type="number"
-            value={enteredAge}
-            onChange={ageChangeHandler}
+            ref={ageInputRef}
           />
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
