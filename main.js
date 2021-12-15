10947f93efe9338251c0eed96e08e9b0e2e01100
iamapurba2003 <new.to.world2003@gmail.com>
2021-12-15 13:00:26 +0530

    Added a Dummy Main.js File

---
+++ main.js
@@ -0,0 +1,7 @@
+const myname = 'John';
+console.log(myname);
+let button = document.querySelector('button');
+button.addEventListener('click', () => {
+    console.log('Clicked');
+}
+);  // end of button event listener
