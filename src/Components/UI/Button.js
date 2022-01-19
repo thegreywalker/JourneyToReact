47d5b90bdd299c5dabe8a7d50ceaaf1a393d0c95
iamapurba2003 <new.to.world2003@gmail.com>
2022-01-19 20:13:28 +0530

    Modified the Button.js file for few Minor Changes

--- src/Components/UI/Button.js
+++ src/Components/UI/Button.js
@@ -5,7 +5,7 @@ const Button = (props) => {
     
     return (
         <button className={styles.button} type={props.type || 'button'} onClick={props.onClick}>
-            {props.children}
+        {props.children}
         </button>
     )
 }
