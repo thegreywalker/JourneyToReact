a3609959d80305350e349c147e5d7920f7857278
iamapurba2003 <new.to.world2003@gmail.com>
2022-01-23 17:59:39 +0530

    feat(Scope:-The-Change-is-on-Scope-Global,-Path:-Packages-files,-lock-files,-src/**): Initial Setup and Added the Header Component along with the Image Banner and Cart Button with the Badge
    Breaking Change: No, Reference Issues: N/A

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Components/Layout/Header";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+    </React.Fragment>
+  );
+}
+
+export default App;
