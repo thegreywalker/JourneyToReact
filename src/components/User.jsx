f8856cd818ff9519f5b0cf96289d8f1f91ffaa65
iamapurba2003 <new.to.world2003@gmail.com>
2022-01-25 15:01:39 +0530

    feat(Scope:-Global-Scope,-Path:-src/**,-src/components/**,-src/store/**): Added Class Based Components & it's ways & Workarounds with Functional Component's features Counterpart
    Addl. : Added Functionals Based Components's FFeatures Counterparts like useState(), useEffect(), useContext() etc. in Class Based Components. And Explored the Possibilities and Scope in Class Based Components and When to use it. A major reason to use a Class Based Components will be resolved in the next Commit ..
    
    BREAKING CHANGE: Yes, REFERENCE ISSUES: N/A, Description: Code Added in w.r.t Class Based Components in respect to it's functional Counterpart Workaround, any small mistake in understanding and implementing features like this keyword and etc. would undermine the job and break th Code. Also, I felt that the useContext(() functional Hook is much better than the "static contextType = <context_name?" class part as it does not provide all the Facilities snd Flexibility the useContext() Hook has to offer.

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,7 +1,17 @@
-import classes from './User.module.css';
+import React from 'react';
+import classes from "./User.module.css";
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+class User extends React.Component {
+  componentWillUnmount() {
+    console.log("User will Unmount!");
+  }
+    render() {
+      return <li className={classes.user}>{this.props.name}</li>;
+    }
+}
+
+// const User = (props) => {
+//   return <li className={classes.user}>{this.props.name}</li>;
+// };
 
 export default User;
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,37 +1,69 @@
-import { useState } from 'react';
-import User from './User';
+import React from "react";
+import User from "./User";
 
-import classes from './Users.module.css';
+import classes from "./Users.module.css";
 
-const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
-];
 
-const Users = () => {
-  const [showUsers, setShowUsers] = useState(true);
 
-  const toggleUsersHandler = () => {
-    setShowUsers((curState) => !curState);
-  };
+class Users extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            showUsers: true,
+            more: "Test",
+        };
+    }
 
-  const usersList = (
-    <ul>
-      {DUMMY_USERS.map((user) => (
-        <User key={user.id} name={user.name} />
-      ))}
-    </ul>
-  );
+    toggleUsersHandler() {
+        this.setState((curState) => {
+            return {
+                showUsers: !curState.showUsers,
+            };
+        });
+    }
 
-  return (
-    <div className={classes.users}>
-      <button onClick={toggleUsersHandler}>
-        {showUsers ? 'Hide' : 'Show'} Users
-      </button>
-      {showUsers && usersList}
-    </div>
-  );
-};
+    render() {
+        const usersList = (
+            <ul>
+                {this.props.users.map((user) => (
+                    <User key={user.id} name={user.name} />
+                ))}
+            </ul>
+        );
+        return (
+            <div className={classes.users}>
+                <button onClick={this.toggleUsersHandler.bind(this)}>
+                    {this.state.showUsers ? "Hide" : "Show"} Users
+                </button>
+                {this.state.showUsers && usersList}
+            </div>
+        );
+    }
+}
+
+// const Users = () => {
+//     const [showUsers, setShowUsers] = React.useState(true);
+
+//     const toggleUsersHandler = () => {
+//         setShowUsers((curState) => !curState);
+//     };
+
+//     const usersList = (
+//         <ul>
+//             {DUMMY_USERS.map((user) => (
+//                 <User key={user.id} name={user.name} />
+//             ))}
+//         </ul>
+//     );
+
+//     return (
+//         <div className={classes.users}>
+//             <button onClick={toggleUsersHandler}>
+//                 {showUsers ? "Hide" : "Show"} Users
+//             </button>
+//             {showUsers && usersList}
+//         </div>
+//     );
+// };
 
 export default Users;
