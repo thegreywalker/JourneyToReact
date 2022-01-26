7e39795e61761fe2d4657f8b39e447133051abef
iamapurba2003 <new.to.world2003@gmail.com>
2022-01-26 15:11:31 +0530

    feat(Scope:-Global,-Path:-Src/**): Code Changes Reflected are for HTTP Requesting Sending using React, fetch() API in JavaScript and ways an Method to Handle Errors and States and Side Effects using React are taught
    Addl. : N/A
    
    BREAKING CHANGE: No, REFERENCE ISSUES: No, Description: Although, not a Breaking Change still it's a Code that affects the Performance of the Application in the long run with the HTTP Requests Section Part, SO writing Efficient Code and Promises and Handling Errors is important or else it will be Problem.

---
+++ src/App.jsx
@@ -0,0 +1,107 @@
+import React from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+  // const dummyMovies = [
+  //   {
+  //     id: 1,
+  //     title: 'Some Dummy Movie',
+  //     openingText: 'This is the opening text of the movie',
+  //     releaseDate: '2021-05-18',
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'Some Dummy Movie 2',
+  //     openingText: 'This is the second opening text of the movie',
+  //     releaseDate: '2021-05-19',
+  //   },
+  // ];
+
+  const [movies, setMovies] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
+
+
+
+  // const fetchMoviesHandler = () => {
+  //   fetch('https://swapi.dev/api/films/').then(response => {
+  //     return response.json();
+  //   }).then(data => {
+  //     const transformedMovies = data.results.map(movieData => {
+  //       return {
+  //         id: movieData.episode_id,
+  //         title: movieData.title,
+  //         openingText: movieData.opening_crawl,
+  //         releaseDate: movieData.release_date
+  //       };
+  //     });
+  //     setMovies(transformedMovies)
+  //   });
+  // }
+
+  const fetchMoviesHandler = React.useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch('https://swapi.dev/api/films/');
+      if (!response.ok) {
+        throw new Error('Something Went Wrong');
+      }
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseData: movieData.release_date,
+        };
+      });
+
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+
+    }
+    setIsLoading(false);
+
+
+  }, []);
+
+  React.useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler])
+
+  let content = <p>Found No Movies!</p>
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />
+  }
+
+  if (error) {
+    content = <p>{error}</p>
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {/* {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && !error && <p>Found no Movies!</p>}
+        {isLoading && <p>Loading...</p>}
+        {!isLoading && error && <p>{error}</p>} */}
+        {content}
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
