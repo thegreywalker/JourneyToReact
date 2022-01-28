ae15b04910cde427527f0add73a03c774bd18100
iamapurba2003 <new.to.world2003@gmail.com>
2022-01-28 21:20:42 +0530

    Added the Code for Adding Custom React Hooks, but due to npm dependency Failure, we could not Complete the Code for the last two lectures. The Link to the official Repository is given below for Reference https://github.com/academind/react-complete-guide-code/tree/15-building-custom-react-hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,42 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Tasks from './components/Tasks/Tasks';
-import NewTask from './components/NewTask/NewTask';
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [tasks, setTasks] = useState([]);
+    const [tasks, setTasks] = React.useState([]);
+    const transformTasks = (taskObj) => {
+        const loadedTasks = [];
+        for (const taskKey in taskObj) {
+            loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+        }
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
-      );
+        setTasks(loadedTasks);
+    };
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
+    const { isLoading, error, sendRequest: fetchTasks } = useHttp({
+        url: "https://react-http-31d76-default-rtdb.firebaseio.com/tasks.json",
+    }, transformTasks);
+    const taskAddHandler = (task) => {
+        setTasks((prevTasks) => prevTasks.concat(task));
+    };
 
-      const data = await response.json();
+    React.useEffect(() => {
+      fetchTasks()
+    }, [])
 
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    fetchTasks();
-  }, []);
-
-  const taskAddHandler = (task) => {
-    setTasks((prevTasks) => prevTasks.concat(task));
-  };
-
-  return (
-    <React.Fragment>
-      <NewTask onAddTask={taskAddHandler} />
-      <Tasks
-        items={tasks}
-        loading={isLoading}
-        error={error}
-        onFetch={fetchTasks}
-      />
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <NewTask onAddTask={taskAddHandler} />
+            <Tasks
+                items={tasks}
+                loading={isLoading}
+                error={error}
+                onFetch={fetchTasks}
+            />
+        </React.Fragment>
+    );
 }
 
 export default App;
