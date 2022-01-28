971dda57138d266c4ff65ddc08f7ed7a616c1732
iamapurba2003 <new.to.world2003@gmail.com>
2022-01-28 14:14:34 +0530

    feat(Scope:-Global,-Path:-src/**): Implemetation of Custom Hooks
    Addl. : Code Changes have Implemented Custom Hooks and their Usage in React, Here we have made out a Custom Hook in React and then re-used it in Two Components, one ForwardCounter & other is the BackwordsCounter. Noting Point is that whenever you make any Custom Hook, make sure you start the name with 'use` and then the Name, as React wants a Guratee that you will abide by the rules of using Hooks even in Your Custom Hooks, Since you can also use React Hooks in your Custom Hooks as well.
    
    BREAKING CHANGE: YES, REFERENCE ISSUE: N/A, Description: Changes made are Syntax Specific. Any changes made if wrongly implemented Could break the Flow and the Way the Application Works.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import BackwardCounter from './components/BackwardCounter';
-import ForwardCounter from './components/ForwardCounter';
+import React from "react";
+import BackwardCounter from "./components/BackwardCounter";
+import ForwardCounter from "./components/ForwardCounter";
 
 function App() {
-  return (
-    <React.Fragment>
-      <ForwardCounter />
-      <BackwardCounter />
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <ForwardCounter />
+            <BackwardCounter />
+        </React.Fragment>
+    );
 }
 
 export default App;
