7b58912e2e375345a1631b8843d50a08d04bbf7c
iamapurba2003 <new.to.world2003@gmail.com>
2022-02-01 22:06:44 +0530

    Cleanup Time

--- with-typescript.js
+++
@@ -1,5 +0,0 @@
-function adds(a, b) {
-    return a + b;
-}
-var results = adds(2, 5);
-console.log(results);
--- with-typescript.ts
+++
@@ -1,7 +0,0 @@
-function adds(a: number, b: number) {
-  return a + b;
-}
-
-const results = adds(2, 5);
-
-console.log(results);
