83509dc1af4751dc27c9a4660425cf635ebf40ba
iamapurba2003 <new.to.world2003@gmail.com>
2022-02-02 03:14:52 +0530

    Making a todo Application using TypeScript & React

---
+++ src/Components/Todos.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Todo from "../models/todo";
+import TodoItem from "./TodoItem";
+
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
+  return (
+    <div>
+      <ul>
+        {props.items.map((item) => (
+          <TodoItem key={item.id} text={item.text} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Todos;
