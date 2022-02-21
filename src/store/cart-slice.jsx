550a3767d575949208547586c8969ad1aaa5f443
iamapurba2003 <new.to.world2003@gmail.com>
2022-02-21 21:14:07 +0530

    Added Cart Toggle Functionality using Redux ( @reduxjs/toolkit, react-redux )

---
+++ src/store/cart-slice.jsx
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+  },
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      if (!existingItem) {
+        state.items.push({
+          ietmID: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          title: newItem.title,
+        });
+      } else {
+        existingItem.quantity += 1;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.totalPrice;
+      }
+    },
+  },
+});
+
+
+export const stateAction = cartSlice.actions;
+export default cartSlice;
