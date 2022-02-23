6709a273ea5eb524730f7ed38d3728710ae9b8f2
iamapurba2003 <new.to.world2003@gmail.com>
2022-02-24 01:42:45 +0530

    Forgot to add Some files ... Commit Referenced to the last  Commit 93c90747

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import classes from './Layout.module.css';
+import MainNavigation from './MainNavigation';
+
+
+const Layout = ({children}) => {
+  return (
+    <React.Fragment>
+        <MainNavigation />
+        <main className={classes.main}>
+          {children}
+        </main>
+    </React.Fragment>
+  )
+}
+
+export default Layout
