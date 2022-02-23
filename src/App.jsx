a3a1d71171ebb39f5ec861ba8d6846019695f355
iamapurba2003 <new.to.world2003@gmail.com>
2022-02-23 19:42:20 +0530

    Added Routing in React Application using React Router ... Also, added functionality of how to extract routes written in URL bar and use it in the Component Specified.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import MainHeader from "./Components/MainHeader";
+import ProductDetail from "./pages/ProductDetail";
+import Products from "./pages/Products";
+import Welcome from "./pages/Welcome";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <MainHeader />
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+        <Route path="/products/:productId">
+          <ProductDetail />
+        </Route>
+      </main>
     </div>
   );
 }
---
+++ src/pages/Welcome.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Welcome = () => {
+  return (
+    <h1>The Welcome Page</h1>
+  )
+}
+
+export default Welcome
