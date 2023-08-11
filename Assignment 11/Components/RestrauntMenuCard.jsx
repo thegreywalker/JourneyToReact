31b2ca98fd1008a2bdc1fb560c69e677a1d56b7a
The Grey Walker <husky-on-line@hotmail.com>
2023-08-11 14:08:59 +0530

    Setup ongoing

--- Assignment 11/Components/RestrauntMenuCard.jsx
+++ Assignment 11/Components/RestrauntMenuCard.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { imageUrl } from "./utils/config";
 
 const RestrauntMenuCard = (props) => {
-    console.log(props);
     return (
         <div className="res-menu">
             <div className="res-menu__left-pane">
